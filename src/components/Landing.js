0710c571e549173a553221ac898abc937c0c03a3
Zixing Li <zl82@cornell.edu>
2017-12-28 23:18:10 -0800

    update navbar margins and add pictures, also updated landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { Grid, Row, Col, Button } from 'react-bootstrap';
-import logo from '../assets/USAF logo.2.jpg'
+import { Grid, Row, Col } from 'react-bootstrap';
+import picture1 from '../assets/picture1.png'
+import picture2 from '../assets/picture2.png'
+import pic1 from '../assets/o-edit.jpg'
+
+const styles = {
+  grid: {
+      paddingLeft: 0,
+      paddingRight: 0
+  },
+  row: {
+      marginLeft: 0,
+      marginRight: 0
+  },
+};
 
 const Landing = () => {
   return (
     <div>
-      <Button bsStyle="default" style={{color:"red"}}>Button Text</Button>
-      <h2>New York Eastside Aikikai</h2>
-      <h3>Martial Arts & Cultural Center</h3>
-      <img src={logo} alt="Aikido Logo" className="logo" />
-      <Grid>
-        <Row>
-          <Col sm={6} md={6}>Test Left</Col>
-          <Col sm={6} md={6}>Test Right</Col>
+      <div className="hero-content">
+        <h2 className="hero-title">New York Eastside Aikikai<br /><br />Martial Arts & Cultural Center</h2>
+      </div>      
+      <Grid fluid style={styles.grid}>
+        <Row style={styles.row}>
+          <Col sm={6} md={6}>
+          <img src={pic1} alt="Aikido Picture" className="banner" />
+          </Col>
+          <Col sm={6} md={6}>
+          NY Eastside Aikikai, located in the Upper Eastside of New York City, offers traditional training in the art of Aikido.  Beginners and advanced practitioners, children and adults alike are invited to discover the joy of natural movement and master effective self-defense techniques.  Find out what Aikido can do for you.
+          </Col>
         </Row>
       </Grid>
     </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Navbar, NavItem, Nav } from 'react-bootstrap';
+import logo from '../assets/USAF logo.2.jpg'
 
 const Navigation = () => {
   return (
     <Navbar>
       <Navbar.Header>
         <Navbar.Brand>
-          <a href="#">React-Bootstrap</a>
+          <a href="#">
+            <img src={logo} alt="Aikido Logo" className="logo" />
+            <h3 className="logo-name">New York Eastside Aikikai</h3>
+          </a>
         </Navbar.Brand>
       </Navbar.Header>
-      <Nav>
+      {/* <Nav>
         <NavItem eventKey={1} href="#">Link</NavItem>
         <NavItem eventKey={2} href="#">Link</NavItem>
-      </Nav>
+      </Nav> */}
     </Navbar>
   )
 }
