533db94100b1a00d32c1265565faab4ab6542d32
Zixing Li <zl82@cornell.edu>
2018-01-06 12:16:41 -0500

    update location page css

--- src/components/Location.js
+++ src/components/Location.js
@@ -6,20 +6,19 @@ const Location = () => {
   return (
     <div className="location">
       <Grid fluid>
-        <Row >
+        <Row className="illustration">
           <Col xs={12} sm={12} md={12} lg={7}>
             <img src={map} alt="map" />
           </Col>
-          <Col xs={12} sm={12} md={12} lg={5}>
-            <div className="location-info">
+          <Col xs={12} sm={12} md={12} lg={5} className="location-info">
+            <span className="illustration-text">
               307 E 92 St, 2 Fl (between 1st & 2nd Avenues), New York NY 10128
-              <br/>
-              By subway:  #4, 5, 6 to 86 St/Lex; #6 to 96 St/Lex
-              <br/>
-              By bus:  M15 to 92 St; M86 to 92 St; M96 to 2nd Ave
-              <br/>
+              <ul>
+                <li>By subway:  #4, 5, 6 to 86 St/Lex; #6 to 96 St/Lex</li>
+                <li>By bus:  M15 to 92 St; M86 to 92 St; M96 to 2nd Ave</li>
+              </ul>
               Visitors are welcome.  Please arrive before regular class times. Thank you.
-            </div>
+            </span>
           </Col>
         </Row>
       </Grid>
