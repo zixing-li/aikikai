9ee3892f2afd449f57523db538cf8cb6a4f5461b
Zixing Li <zl82@cornell.edu>
2018-02-05 23:30:51 -0500

    add postcard and support 2560 screens

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import fee from '../assets/images/fee.jpg';
+import postcard from '../assets/images/2018 New Trial Postcard.jpg';
 
 const Schedule = () => {
   return (
@@ -13,49 +14,57 @@ const Schedule = () => {
           <br/>
           Promotion tests are conducted three times a year for both adults and children. Aikido is non-competitive, and testing is optional; however, if you wish to be promoted, it is important to attend class regularly. In order to make progress, students are encouraged to train at least three times a week. Adult kyu ranking (white belts) is awarded by USAF, and upon reaching black belt level, the rank is awarded by the Aikikai Foundation, Tokyo, Japan.
         </div>
-        Schedule <hr/>
-        <table>
-          <tr>
-            <th></th>
-            <th>Mon</th>
-            <th>Tue</th>
-            <th>Wed</th>
-            <th>Thu</th>
-            <th>Fri</th>
-          </tr>
-          <tr>
-            <td><b>5:30-6:15p</b></td>
-            <td></td>
-            <td>Kids (age 8-14) Johnston</td>
-            <td></td>
-            <td>Kids (age 8-14) Mena</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td><b>7:00-8:00p</b></td>
-            <td>All levels Johnston</td>
-            <td>All levels Yamazaki</td>
-            <td>All levels Mena/Stevens</td>
-            <td>All levels Yamazaki</td>
-            <td>All levels Yamazaki</td>
-          </tr>
-        </table>
-        <table>
-          <tr>
-            <th></th>
-            <th>Sat</th>
-          </tr>
-          <tr>
-            <td><b>10:30-11:15a</b></td>
-            <td>Kids (age 8-14) Stevens</td>
-          </tr>
-          <tr>
-            <td><b>11:30-1:00p</b></td>
-            <td>Aikido&Weapons Yamazaki</td>
-          </tr>
-        </table>
-        Fees <hr/>
-        <img src={fee} alt="fee"/>
+        <div className="schedule-section">
+          Schedule <hr/>
+          <table>
+            <tr>
+              <th></th>
+              <th>Mon</th>
+              <th>Tue</th>
+              <th>Wed</th>
+              <th>Thu</th>
+              <th>Fri</th>
+            </tr>
+            <tr>
+              <td><b>5:30-6:15p</b></td>
+              <td></td>
+              <td>Kids (age 8-14) Johnston</td>
+              <td></td>
+              <td>Kids (age 8-14) Mena</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td><b>7:00-8:00p</b></td>
+              <td>All levels Johnston</td>
+              <td>All levels Yamazaki</td>
+              <td>All levels Mena/Stevens</td>
+              <td>All levels Yamazaki</td>
+              <td>All levels Yamazaki</td>
+            </tr>
+          </table>
+          <table>
+            <tr>
+              <th></th>
+              <th>Sat</th>
+            </tr>
+            <tr>
+              <td><b>10:30-11:15a</b></td>
+              <td>Kids (age 8-14) Stevens</td>
+            </tr>
+            <tr>
+              <td><b>11:30-1:00p</b></td>
+              <td>Aikido&Weapons Yamazaki</td>
+            </tr>
+          </table>
+        </div>
+        <div className="fee-section">
+          Fees <hr/>
+          <img src={fee} alt="fee"/>
+        </div>
+        <div className="postcard-section">
+          Trial Postcard <hr/>
+          <img src={postcard} alt="postcard"/>
+        </div>
       </div>
     </div>
   )
