5550941fa3b4064b0770acf18a3755866f8b3166
Zixing Li <zl82@cornell.edu>
2018-02-06 22:34:07 -0500

    update location text style

--- src/components/Location.js
+++ src/components/Location.js
@@ -11,14 +11,14 @@ const Location = () => {
             <img src={map} alt="map" />
           </Col>
           <Col xs={12} sm={12} md={12} lg={5} className="location-info">
-            <span className="illustration-text">
+            <p>
               307 E 92 St, 2 Fl (between 1st & 2nd Avenues), New York NY 10128
               <ul>
                 <li>By subway:  #4, 5, 6 to 86 St/Lex; #6 to 96 St/Lex; Q train to 96th St and 2nd Ave</li>
                 <li>By bus:  M15 to 92 St; M86 to 92 St; M96 to 2nd Ave</li>
               </ul>
               Visitors are welcome.  Please arrive before regular class times. Thank you.
-            </span>
+            </p>
           </Col>
         </Row>
       </Grid>
