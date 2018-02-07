09ef3953a1a2d1824136bb6d9224347b6073f67d
Zixing Li <zl82@cornell.edu>
2018-02-06 20:55:24 -0500

    change margin and paddings for small and large screens

--- src/components/Aikido.js
+++ src/components/Aikido.js
@@ -7,11 +7,9 @@ const Aikido = () => {
         <h2 className="banner-title">Aikido</h2>
       </div>
       <div className="aikido-description">
-        Aikido is a Japanese martial art founded in the early 20th century by Morihei Ueshiba (1883-1969), also known as O-Sensei (Great Teacher). O-Sensei was a master of several Samurai arts including Kenjutsu (sword), Sojutsu (spear) and Jujutsu. He was a deeply spiritual person and realized that peaceful resolutions were necessary for ending violence in the world. O-Sensei ultimately created a martial art in which aggression is met with compassion. Aikido is sometimes referred to as the "way of harmony."
-        <br/>
-        Aikido is primarily an unarmed art; however, there is substantial training with the sword and staff. Movement is characterized as dynamic, circular and efficient. Attacks are resolved through maintaining proper timing, distance, and feeling; conflict is avoided. With daily practice the body will become stronger and endurance greater. Along with the practical application of Aikido as a form of self-defense, the training can serve to improve one's quality of life by following the basic principles of Aikido: relax completely, stay calm, keep your center, and stay positive. As students continue to practice Aikido, they experience a transformation in mind, body, and spirit.
-        <br/>
-        Aikido is a martial art, but it does not encourage fighting; the focus is on developing self-protection skills and finding peaceful resolutions.
+        <p>Aikido is a Japanese martial art founded in the early 20th century by Morihei Ueshiba (1883-1969), also known as O-Sensei (Great Teacher). O-Sensei was a master of several Samurai arts including Kenjutsu (sword), Sojutsu (spear) and Jujutsu. He was a deeply spiritual person and realized that peaceful resolutions were necessary for ending violence in the world. O-Sensei ultimately created a martial art in which aggression is met with compassion. Aikido is sometimes referred to as the "way of harmony."</p>
+        <p>Aikido is primarily an unarmed art; however, there is substantial training with the sword and staff. Movement is characterized as dynamic, circular and efficient. Attacks are resolved through maintaining proper timing, distance, and feeling; conflict is avoided. With daily practice the body will become stronger and endurance greater. Along with the practical application of Aikido as a form of self-defense, the training can serve to improve one's quality of life by following the basic principles of Aikido: relax completely, stay calm, keep your center, and stay positive. As students continue to practice Aikido, they experience a transformation in mind, body, and spirit.</p>
+        <p>Aikido is a martial art, but it does not encourage fighting; the focus is on developing self-protection skills and finding peaceful resolutions.</p>
       </div>
     </div>
   )
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -10,9 +10,8 @@ const Schedule = () => {
       </div>
       <div className="schedule-fees-section">
         <div className="schedule-info">
-          Prospective students should observe a class before deciding to join as a member. Upon becoming a member, beginners may undertake their study at any time during the month. Classes are unlimited for members.
-          <br/>
-          Promotion tests are conducted three times a year for both adults and children. Aikido is non-competitive, and testing is optional; however, if you wish to be promoted, it is important to attend class regularly. In order to make progress, students are encouraged to train at least three times a week. Adult kyu ranking (white belts) is awarded by USAF, and upon reaching black belt level, the rank is awarded by the Aikikai Foundation, Tokyo, Japan.
+          <p>Prospective students should observe a class before deciding to join as a member. Upon becoming a member, beginners may undertake their study at any time during the month. Classes are unlimited for members.</p>
+          <p>Promotion tests are conducted three times a year for both adults and children. Aikido is non-competitive, and testing is optional; however, if you wish to be promoted, it is important to attend class regularly. In order to make progress, students are encouraged to train at least three times a week. Adult kyu ranking (white belts) is awarded by USAF, and upon reaching black belt level, the rank is awarded by the Aikikai Foundation, Tokyo, Japan.</p>
         </div>
         <div className="schedule-section">
           Schedule <hr/>
