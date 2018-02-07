25428fe952b904399fff6faef6dc1b6ac69339db
Zixing Li <zl82@cornell.edu>
2018-02-06 22:17:15 -0500

    update images

--- src/components/Instructors.js
+++ src/components/Instructors.js
@@ -2,6 +2,7 @@ import React from 'react';
 import sensei from '../assets/images/Sensei.jpg';
 import kim from '../assets/images/Kim.jpg';
 import nacho from '../assets/images/Nacho.jpg';
+import elizabeth from '../assets/images/Elizabeth.jpg';
 
 const Instructors = () => {
   return (
@@ -13,24 +14,30 @@ const Instructors = () => {
         <img src={sensei} alt="Sensei" />
         <div style={{textAlign:"center", marginTop:"10px"}}>
           <p><b>Chief Instructor</b></p>
-          <p>Ken Yamazaki Sensei, Godan & Shidoin</p>
+          <p>Ken Yamazaki</p>
         </div>
-        <p>Biography: Ken Yamazaki, 5th dan Aikikai, is the chief instructor of NY Eastside Aikikai, affiliated with United States Aikido Federation. Yamazaki Sensei began Aikido and Zen Buddhism studies in 1989 with Reverend Joseph Jarman, a Buddhist priest and jazz musician; he then continued his training with Doshu Kisshomaru Ueshiba, Fumio Toyoda and many prominent Aikido shihans in Japan and the U.S.  From 2005 to 2012 he was a direct student and uchideshi of Yasuo Kobayashi Shihan, 8th Dan, disciple of Aikido founder, Morihei Ueshiba.  At present, Yamazaki Sensei is a direct student of Yoshimitsu Yamada Shihan, 8th Dan, also a disciple of Morihei Ueshiba.</p>
-        <p>Yamazaki Sensei has been instructing Aikido since 1992 and, prior to becoming a Shidoin for U.S. Aikido Federation, has served on the teaching staff of Aikido Association of America and Aikido Kobayashi Dojo.  He is also a world-renowned jazz musician who has been performing professionally since 1978.</p>
+        <p>Biography: Ken Yamazaki, 5th dan Aikikai, Shidoin, is the chief instructor of NY Eastside Aikikai. He began Aikido and Zen Buddhism studies in 1989 with Reverend Joseph Jarman.  Yamazaki Sensei was also a longtime student of Fumio Toyoda Shihan, and uchideshi (live-in student) of Yasuo Kobayashi Shihan; he has also trained with Doshu Kisshomaru Ueshiba at Aikikai Hombu Dojo; at present, Yamazaki Sensei is a direct student of Yoshimitsu Yamada Shihan, a pioneer in introducing Aikido to North America.  Yamazaki Sensei is also a world-renowned jazz musician, who has performed with a long list of who's who in jazz..</p>
         <br/>
         <img src={kim} alt="Kim" />
         <div style={{textAlign:"center", marginTop:"10px"}}>
           <p><b>Instructor, Children’s Program</b></p>
-          <p>Kim Johnston, Sandan & Fukushidoin</p>
+          <p>Kim Johnston</p>
         </div>
-        <p>Biography: Kim Johnston, 3rd dan Aikikai, is the instructor for the children’s Aikido program at NY Eastside Aikikai. She has been practicing Aikido for over a decade. Kim is also a Shiatsu practitioner.  Certified by the Ohashi Institute since 1997, Kim was a Shiatsu instructor at the Ohashi Institute as well as the Pacific College of Oriental Medicine. Kim also holds an MFA in modern dance.</p>
+        <p>Biography: Kim Johnston, 3rd Dan Aikikai, Fukushidoin, is the head instructor for the children's program at NYEA.  Kim's classes are fun and energetic, and focus on the personal development of each student.  Kim is also a Shiatsu practitioner (certified by the Ohashi Institute since 1997), and holds an MFA in modern dance.</p>
         <br/>
         <img src={nacho} alt="Nacho" />
         <div style={{textAlign:"center", marginTop:"10px"}}>
           <p><b>Instructor</b></p>
-          <p>Nacho Mena, Shodan</p>
+          <p>Nacho Mena</p>
         </div>
-        <p>Biography:  Nacho Mena, 1st dan Aikikai.  After practicing Judo as a child in Spain, Nacho Mena started learning Aikido when he moved to San Diego in 1996, to work as a Molecular Virologist. He practiced with Coryl Crane Shihan, a student of Kazuo Chiba Shihan, 8th dan. After three years in San Diego, Nacho went to work in Paris, France, where he continued the practice of Aikido with the Birankai Association, founded by students of Chiba Sensei, and obtained the 1st dan Aikikai in 2003. After a break of several years, in which he returned to work in Spain and to the Judo practice, Nacho arrived in New York in 2011, and joined the NY Eastside Aikikai in September 2014.</p>
+        <p>Biography: Nacho Mena, 2nd Dan Aikikai, is an instructor at NYEA; he began Aikido in 1996 with Coryl Crane Shihan, a student of Kazuo Chiba Shihan, 8th Dan.  Nacho is a molecular virologist, and also holds a 4th Dan in Judo.</p>
+        <br/>
+        <img src={elizabeth} alt="Elizabeth" />
+        <div style={{textAlign:"center", marginTop:"10px"}}>
+          <p><b>Instructor</b></p>
+          <p>Elizabeth Stevens</p>
+        </div>
+        <p>Biography: Elizabeth Stevens, 1st Dan Aikikai, is an instructor at NYEA; she began Aikido in 2011 with Yamazaki Sensei.  Elizabeth is also an accomplished educator at a prestigious private school in NYC.</p>
       </div>
     </div>
   )
